63a5f6363e1e2e4aa3f32b0f3fbd5fae0d7d1f26
Alina Stewart <alinastewart@Alinas-iMac.local>
2021-01-04 15:45:15 -0800

    Convert string to all lowercase or all uppercase letters

---
+++ myLowerUpper.java
@@ -0,0 +1,9 @@
+
+
+public class myLowerUpper {
+  public static void main(String[] args) {
+    String txt = "Hello World";
+    System.out.println(txt.toLowerCase());
+    System.out.println(txt.toUpperCase());
+  }
+}
